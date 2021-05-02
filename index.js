df7ff168f28186bcb1d7bed935860f32b3fb27de
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-05-02 18:47:49 +0200

    front end update, some more text

---
+++ index.js
@@ -0,0 +1,10 @@
+import moduleName from 'module';
+
+const doms = {
+  mobileMenuButton: document.querySelector('#mobile'),
+};
+
+const mobileMenuClickHandler = event => {};
+
+//event listeners
+addEventListener(doms.mobileMenuButton, 'click', mobileMenuClickHandler);
